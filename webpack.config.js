4c24a0761f4da7af805f28bbdb42f3aff1fae8c1
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-02-28 08:43:57 +0000

    Switch sass compilation across to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,75 @@
 var glob = require('glob')
 var path = require('path')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
+var extend = require('extend')
 
 var config = require('./gulp/config.js')
 
-var jsEntryPoints = glob.sync(path.join(config.sourcePath, 'javascripts/*.js'))
-
-jsEntryPoints = jsEntryPoints.reduce((accumulator, value) => {
-  accumulator[path.basename(value, '.js')] = path.resolve(value)
-  return accumulator
-}, {})
-
-module.exports = {
+var baseWebpackConfig = {
   mode: 'production',
   bail: true,
   devtool: 'source-map',
-  entry: jsEntryPoints,
   output: {
-    path: path.resolve(path.join(config.destinationPath, 'javascripts')),
-    filename: '[name].js'
+    path: path.resolve(config.destinationPath)
   },
   resolve: {
     modules: config.nodePath
   },
   resolveLoader: {
     modules: config.nodePath
+  },
+  watchOptions: {
+    poll: true // inotify doesn't work inside VMs
+  },
+  stats: {
+    assets: true,
+    colors: true,
+    entrypoints: false,
+    hash: false,
+    modules: false,
+    version: false
+  }
+}
+
+// Workaround for https://github.com/webpack/webpack/issues/7300
+// See exact solution at https://github.com/webpack/webpack/issues/7300#issuecomment-413959996
+// Without this workaround, the css extract solution leaves behind empty JavaScript files
+class MiniCssExtractPluginCleanUp {
+  constructor (deleteWhere = /\.js(\.map)?$/) {
+    this.shouldDelete = new RegExp(deleteWhere)
+  }
+  apply (compiler) {
+    compiler.hooks.emit.tapAsync('MiniCssExtractPluginCleanup', (compilation, callback) => {
+      Object.keys(compilation.assets).forEach((asset) => {
+        if (this.shouldDelete.test(asset)) {
+          delete compilation.assets[asset]
+        }
+      })
+      callback()
+    })
+  }
+}
+
+var jsEntryPoints
+var jsFiles = glob.sync(path.join(config.sourcePath, 'javascripts/*.js'))
+
+jsEntryPoints = jsFiles.reduce((accumulator, value) => {
+  accumulator[path.join('js', path.basename(value, '.js'))] = path.resolve(value)
+  return accumulator
+}, {})
+
+var scssFiles = glob.sync(path.join(config.sourcePath, 'scss/*.scss'))
+var scssEntryPoints
+
+scssEntryPoints = scssFiles.reduce((accumulator, value) => {
+  accumulator[path.join('css', path.basename(value, '.scss'))] = path.resolve(value)
+  return accumulator
+}, {})
+
+var jsWebpackConfig = {
+  entry: jsEntryPoints,
+  output: {
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -40,16 +86,46 @@ module.exports = {
         }
       }
     ]
-  },
-  watchOptions: {
-    poll: true // inotify doesn't work inside VMs
-  },
-  stats: {
-    assets: true,
-    colors: true,
-    entrypoints: false,
-    hash: false,
-    modules: false,
-    version: false
   }
 }
+
+var scssWebpackConfig = {
+  entry: scssEntryPoints,
+  output: {
+    filename: '[name].css'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?[ac]ss$/,
+        resolve: {
+          extensions: ['.scss', '.sass']
+        },
+        use: [
+          // {
+          //   loader: 'file-loader'
+          // },
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader?-url'
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new MiniCssExtractPluginCleanUp()
+  ]
+}
+
+module.exports = [
+  extend(scssWebpackConfig, baseWebpackConfig),
+  extend(jsWebpackConfig, baseWebpackConfig)
+]
