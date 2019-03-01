2b51c8572275c178a5f223a9bec9afefa2d50316
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-03-01 15:54:49 +0000

    Fixing header logo in the react component

--- demo/assets/src/javascripts/react-components.js
+++ demo/assets/src/javascripts/react-components.js
@@ -1,3 +1,5 @@
 import { ReactDOM } from 'react-dom'
 import Header from '../../../../hmlr_design_system/components/header/component'
 window.Header = Header
+
+window.application_config = JSON.parse(document.getElementById('application_config').innerHTML)
--- hmlr_design_system/components/header/component.js
+++ hmlr_design_system/components/header/component.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
 function Header (props) {
+  const Logo = (
+
+    // TODO: Consider paths to images. React way would be to use file-loader
+    // Just do it twice?!
+
+    <img className='hmlr-header__logo'
+      src={`${window.application_config.static_path}images/hmlr-hmlr-design-system/header/logo.svg`}
+      alt='HM Land Registry' />
+  )
+
   return <>
-    <p>This is from React</p>
+    <header className='hmlr-header'>
+      <div className='hmlr-header__inner'>
+        { props.homepageUrl
+          ? (
+            <a href={props.homepageUrl} className='hmlr-header__link'>
+              Logo
+            </a>
+          ) : (
+            Logo
+          )
+        }
+      </div>
+    </header>
   </>
 }
 
