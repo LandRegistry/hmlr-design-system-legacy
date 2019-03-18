c8d197a36b5dcf7a4bfd9e306e3ac96759883bac
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-03-18 09:33:23 +0000

    Refactoring footer component

--- hmlr_design_system/components/footer/component.js
+++ hmlr_design_system/components/footer/component.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
 function Footer (props) {
+  const links = props.children && <>
+    <h2 className='govuk-visually-hidden'>Support links</h2>
+    <ul className='hmlr-footer__links'>
+      { React.Children.map(props.children, (item, index) => (
+        <li className='hmlr-footer__item' key={index}>
+          {React.cloneElement(item, { className: 'hmlr-footer__link' })}
+        </li>
+      ))}
+    </ul>
+  </>
+
   return <footer className='hmlr-footer'>
     <div className='hmlr-footer__inner'>
-      { props.children && (
-        <>
-          <h2 className='govuk-visually-hidden'>Support links</h2>
-          <ul className='hmlr-footer__links'>
-            { React.Children.map(props.children, (item, index) => (
-              <li className='hmlr-footer__item' key={index}>
-                {React.cloneElement(item, { className: 'hmlr-footer__link' })}
-              </li>
-            ))}
-          </ul>
-        </>
-      )}
+      { links}
     </div>
   </footer>
 }
