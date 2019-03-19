ffe9aa064dfedb9595ed30dbca7b9746cec9ab63
Michael Jones <michaelj@kainos.com>
2019-03-19 16:21:25 +0000

    fixed internal header

--- demo/assets/src/javascripts/react-components.js
+++ demo/assets/src/javascripts/react-components.js
@@ -1,6 +1,6 @@
 import { ReactDOM } from 'react-dom'
 
-import Header from '../../../../hmlr_design_system/components/header/component'
+import Header from '../../../../hmlr_design_system/components/header-internal/component'
 window.Header = Header
 
 import Footer from '../../../../hmlr_design_system/components/footer/component'
