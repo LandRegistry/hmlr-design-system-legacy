c159b3bed355321942673868ff42d245985eab8f
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-04-18 15:08:05 +0100

    Further work on refactored headers

--- hmlr_design_system/components/header/component.js
+++ hmlr_design_system/components/header/component.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import logoImage from './logo.svg'
+import whiteLogoImage from './logo-white.svg'
 
 function Header(props) {
-  const Logo = (
-    <img className='hmlr-header__logo'
-      src={logoImage}
-      alt='HM Land Registry' />
-  )
+  const logoPath = props.variant == 'dark' ? whiteLogoImage : logoImage
+  const logo = <img className='hmlr-header__logo' src={logoPath} alt='HM Land Registry' />
 
-  return <header className='hmlr-header'>
+  const headerTitle = props.headerTitle && <span className="hmlr-header__title">{props.headerTitle}</span>
+
+  const contents = <>
+    {props.logo && logo}
+    {headerTitle}
+  </>
+
+  return <header className={`hmlr-header hmlr-header--${props.variant}`}>
     <div className='hmlr-header__inner'>
       {props.homepageUrl
         ? (
           <a href={props.homepageUrl} className='hmlr-header__link'>
-            Logo
+            contents
           </a>
         ) : (
-          Logo
+          contents
         )
       }
     </div>
   </header>
+}
+
+Header.defaultProps = {
+  variant: 'white'
 }
 
 export default Header
