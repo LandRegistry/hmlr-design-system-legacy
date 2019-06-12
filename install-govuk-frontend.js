d6554c753e0b39abf33bd346ab31d5b59059ff67
Andy Mantell <134642+andymantell@users.noreply.github.com>
2019-06-12 15:52:47 +0100

    Move gov kit to a peer dependency and update it

---
+++ install-govuk-frontend.js
@@ -0,0 +1,20 @@
+const exec = require('child_process').execSync
+const path = require('path')
+
+const packageJson = require('./package.json')
+// Check if we've got govuk-frontend installed and install it if not
+try {
+  require.resolve('govuk-frontend')
+} catch {
+  console.log('govuk-frontend not found, installing...')
+  exec('npm install --no-save govuk-frontend@' + packageJson.peerDependencies['govuk-frontend'])
+}
+
+// Check we've got the right version, and die if we don't
+const govukFrontendPath = path.dirname(require.resolve('govuk-frontend/README.md'))
+const govPackageJson = require(path.join(govukFrontendPath, 'package.json'))
+if(govPackageJson.version !== packageJson.peerDependencies['govuk-frontend']) {
+  console.error('govuk-frontend', govPackageJson.version, 'found. Expected', packageJson.peerDependencies['govuk-frontend'])
+  console.error('Please delete node_modules and npm install again.')
+  process.exit(1)
+}
