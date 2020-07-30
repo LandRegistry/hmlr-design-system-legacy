db9cb55e0775f744c98b4e4830436dff3234f2f3
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-07-30 15:33:19 +0100

    remove gov.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,7 +3,6 @@ const gulp = require('gulp')
 const config = require('./gulp/config.js')
 
 require('./gulp/tasks/clean.js')(gulp, config)
-require('./gulp/tasks/gov.js')(gulp, config)
 require('./gulp/tasks/images.js')(gulp, config)
 require('./gulp/tasks/linting.js')(gulp, config)
 require('./gulp/tasks/default.js')(gulp, config)
